02091c842564c4e3f33ee05af5781dfdca6eaf94
berkansivri <berkansivri@gmail.com>
2019-05-07 17:16:28 +0300

    $apollo.query usage sample

--- start/client-vue/src/App.vue
+++ start/client-vue/src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img width="50" alt="Vue logo" src="./assets/logo.png">
+    <br />
     <p>{{hello}}</p>
+    <div role="group">
+      <button type="button" @click="getLaunches">Get Launches</button>
+      <button type="button">Get Launch By Id</button>
+      <button type="button">Me</button>
+    </div>
+    <p v-for="launch in launches">{{launch.site}}</p>
   </div>
 </template>
 
@@ -11,14 +18,40 @@ import gql from "graphql-tag";
 export default {
   name: "app",
   data() {
-    return {};
+    return {
+      launch: {},
+      launches: {}
+    };
+  },
+  methods: {
+    getLaunches() {
+      console.log("get");
+      var data = this.$apollo.query({
+        query: gql`
+          query {
+            launches {
+              site
+            }
+          }
+        `
+      });
+      console.log(data.then(val => {console.log(val)}));
+      // console.log(data);
+    }
   },
   apollo: {
     hello: gql`
       query {
         hello
       }
-    `
+    `,
+    launch: gql`
+      query {
+        launch(id: 1) {
+          site
+        }
+      }
+    `, 
   }
 };
 </script>
